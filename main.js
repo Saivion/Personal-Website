6fb7a165cc156743ee625f76c9f0e708792cfa72
Saivion Hayes <saivionhayes11@gmail.com>
2020-11-10 16:33:58 -0600

    updates --

--- main.js
+++ main.js
@@ -19,12 +19,38 @@ function myFunction(activeTabs) {
     if (activeTabs == 'student') {
         document.getElementById("student").style.display = '';
         document.getElementById("portfolio").style.display = 'none';
+        document.getElementById("blog").style.display = 'none';
         // document.getElementById("experience").style.display = 'none';
 
         document.getElementById("aboutStudent").style.textDecoration = 'none'
         document.getElementById("aboutStudent").style.color = '#333333'
         document.getElementById("aboutStudent").style.fontWeight = '500'
         document.getElementById("aboutStudent").style.fontFamily = 'Poppins, sans-serif'
+        document.getElementById("aboutBlog").style.textDecoration = 'none'
+        document.getElementById("aboutBlog").style.color = '#999999'
+        document.getElementById("aboutBlog").style.fontWeight = ''
+        document.getElementById("aboutBlog").style.fontFamily = 'Poppins, sans-serif'
+        document.getElementById("aboutPortfolio").style.textDecoration = 'none'
+        document.getElementById("aboutPortfolio").style.color = '#999999'
+        document.getElementById("aboutPortfolio").style.fontWeight = ''
+        document.getElementById("aboutPortfolio").style.fontFamily = 'Poppins, sans-serif'
+        // document.getElementById("aboutExperience").style.textDecoration = 'none'
+        // document.getElementById("aboutExperience").style.color = 'none;'
+    }
+    if (activeTabs == 'blog') {
+        document.getElementById("student").style.display = 'none';
+        document.getElementById("portfolio").style.display = 'none';
+        document.getElementById("blog").style.display = '';
+        // document.getElementById("experience").style.display = 'none';
+
+        document.getElementById("aboutStudent").style.textDecoration = 'none'
+        document.getElementById("aboutStudent").style.color = '#999999'
+        document.getElementById("aboutStudent").style.fontWeight = ''
+        document.getElementById("aboutStudent").style.fontFamily = 'Poppins, sans-serif'
+        document.getElementById("aboutBlog").style.textDecoration = 'none'
+        document.getElementById("aboutBlog").style.color = '#333333'
+        document.getElementById("aboutBlog").style.fontWeight = '500'
+        document.getElementById("aboutBlog").style.fontFamily = 'Poppins, sans-serif'
         document.getElementById("aboutPortfolio").style.textDecoration = 'none'
         document.getElementById("aboutPortfolio").style.color = '#999999'
         document.getElementById("aboutPortfolio").style.fontWeight = ''
@@ -35,12 +61,17 @@ function myFunction(activeTabs) {
     if (activeTabs == 'portfolio') {
         document.getElementById("student").style.display = 'none';
         document.getElementById("portfolio").style.display = '';
+        document.getElementById("blog").style.display = 'none';
         // document.getElementById("experience").style.display = 'none';
 
         document.getElementById("aboutStudent").style.textDecoration = 'none'
         document.getElementById("aboutStudent").style.color = '#999999'
         document.getElementById("aboutStudent").style.fontWeight = ''
         document.getElementById("aboutStudent").style.fontFamily = 'Poppins, sans-serif'
+        document.getElementById("aboutBlog").style.textDecoration = 'none'
+        document.getElementById("aboutBlog").style.color = '#999999'
+        document.getElementById("aboutBlog").style.fontWeight = ''
+        document.getElementById("aboutBlog").style.fontFamily = 'Poppins, sans-serif'
         document.getElementById("aboutPortfolio").style.textDecoration = 'none'
         document.getElementById("aboutPortfolio").style.color = '#333333'
         document.getElementById("aboutPortfolio").style.fontWeight = '500'
