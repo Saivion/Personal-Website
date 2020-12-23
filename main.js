7d9dbe4dedb5a62c85a7a5c411293be100c93652
Saivion Hayes <saivionhayes11@gmail.com>
2020-12-23 16:22:22 -0600

    Revert "updates --"
    This reverts commit bb7c1417ba1bdcd8c28df3e03e16efb1f270ebae.

--- main.js
+++ main.js
@@ -18,24 +18,6 @@
 
 // Tabs 
 function myFunction(activeTabs) {
-    if (activeTabs == 'portfolio') {
-        document.getElementById("student").style.display = 'none';
-        document.getElementById("portfolio").style.display = '';
-        document.getElementById("blog").style.display = 'none';
-        // document.getElementById("experience").style.display = 'none';
-
-        document.getElementById("aboutStudent").style.textDecoration = 'none'
-        document.getElementById("aboutStudent").style.fontWeight = ''
-        document.getElementById("aboutStudent").style.fontFamily = 'Poppins, sans-serif'
-        document.getElementById("aboutBlog").style.textDecoration = 'none'
-        document.getElementById("aboutBlog").style.fontWeight = ''
-        document.getElementById("aboutBlog").style.fontFamily = 'Poppins, sans-serif'
-        document.getElementById("aboutPortfolio").style.textDecoration = 'none'
-        document.getElementById("aboutPortfolio").style.fontWeight = '500'
-        document.getElementById("aboutPortfolio").style.fontFamily = 'Poppins, sans-serif'
-        // document.getElementById("aboutExperience").style.textDecoration = 'none'
-        // document.getElementById("aboutExperience").style.color = 'rgb(34, 34, 34);'
-    }
     if (activeTabs == 'student') {
         document.getElementById("student").style.display = '';
         document.getElementById("portfolio").style.display = 'none';
@@ -43,39 +25,62 @@ function myFunction(activeTabs) {
         // document.getElementById("experience").style.display = 'none';
 
         document.getElementById("aboutStudent").style.textDecoration = 'none'
+        document.getElementById("aboutStudent").style.color = '#333333'
         document.getElementById("aboutStudent").style.fontWeight = '500'
         document.getElementById("aboutStudent").style.fontFamily = 'Poppins, sans-serif'
         document.getElementById("aboutBlog").style.textDecoration = 'none'
+        document.getElementById("aboutBlog").style.color = '#999999'
         document.getElementById("aboutBlog").style.fontWeight = ''
         document.getElementById("aboutBlog").style.fontFamily = 'Poppins, sans-serif'
         document.getElementById("aboutPortfolio").style.textDecoration = 'none'
+        document.getElementById("aboutPortfolio").style.color = '#999999'
         document.getElementById("aboutPortfolio").style.fontWeight = ''
         document.getElementById("aboutPortfolio").style.fontFamily = 'Poppins, sans-serif'
         // document.getElementById("aboutExperience").style.textDecoration = 'none'
         // document.getElementById("aboutExperience").style.color = 'none;'
     }
-    // if (activeTabs == 'blog') {
-    //     document.getElementById("student").style.display = 'none';
-    //     document.getElementById("portfolio").style.display = 'none';
-    //     document.getElementById("blog").style.display = '';
-    //     // document.getElementById("experience").style.display = 'none';
+    if (activeTabs == 'blog') {
+        document.getElementById("student").style.display = 'none';
+        document.getElementById("portfolio").style.display = 'none';
+        document.getElementById("blog").style.display = '';
+        // document.getElementById("experience").style.display = 'none';
 
-    //     document.getElementById("aboutStudent").style.textDecoration = 'none'
-    //     document.getElementById("aboutStudent").style.color = '#999999'
-    //     document.getElementById("aboutStudent").style.fontWeight = ''
-    //     document.getElementById("aboutStudent").style.fontFamily = 'Poppins, sans-serif'
-    //     document.getElementById("aboutBlog").style.textDecoration = 'none'
-    //     document.getElementById("aboutBlog").style.color = '#333333'
-    //     document.getElementById("aboutBlog").style.fontWeight = '500'
-    //     document.getElementById("aboutBlog").style.fontFamily = 'Poppins, sans-serif'
-    //     document.getElementById("aboutPortfolio").style.textDecoration = 'none'
-    //     document.getElementById("aboutPortfolio").style.color = '#999999'
-    //     document.getElementById("aboutPortfolio").style.fontWeight = ''
-    //     document.getElementById("aboutPortfolio").style.fontFamily = 'Poppins, sans-serif'
-    //     // document.getElementById("aboutExperience").style.textDecoration = 'none'
-    //     // document.getElementById("aboutExperience").style.color = 'none;'
-    // }
+        document.getElementById("aboutStudent").style.textDecoration = 'none'
+        document.getElementById("aboutStudent").style.color = '#999999'
+        document.getElementById("aboutStudent").style.fontWeight = ''
+        document.getElementById("aboutStudent").style.fontFamily = 'Poppins, sans-serif'
+        document.getElementById("aboutBlog").style.textDecoration = 'none'
+        document.getElementById("aboutBlog").style.color = '#333333'
+        document.getElementById("aboutBlog").style.fontWeight = '500'
+        document.getElementById("aboutBlog").style.fontFamily = 'Poppins, sans-serif'
+        document.getElementById("aboutPortfolio").style.textDecoration = 'none'
+        document.getElementById("aboutPortfolio").style.color = '#999999'
+        document.getElementById("aboutPortfolio").style.fontWeight = ''
+        document.getElementById("aboutPortfolio").style.fontFamily = 'Poppins, sans-serif'
+        // document.getElementById("aboutExperience").style.textDecoration = 'none'
+        // document.getElementById("aboutExperience").style.color = 'none;'
+    }
+    if (activeTabs == 'portfolio') {
+        document.getElementById("student").style.display = 'none';
+        document.getElementById("portfolio").style.display = '';
+        document.getElementById("blog").style.display = 'none';
+        // document.getElementById("experience").style.display = 'none';
 
+        document.getElementById("aboutStudent").style.textDecoration = 'none'
+        document.getElementById("aboutStudent").style.color = '#999999'
+        document.getElementById("aboutStudent").style.fontWeight = ''
+        document.getElementById("aboutStudent").style.fontFamily = 'Poppins, sans-serif'
+        document.getElementById("aboutBlog").style.textDecoration = 'none'
+        document.getElementById("aboutBlog").style.color = '#999999'
+        document.getElementById("aboutBlog").style.fontWeight = ''
+        document.getElementById("aboutBlog").style.fontFamily = 'Poppins, sans-serif'
+        document.getElementById("aboutPortfolio").style.textDecoration = 'none'
+        document.getElementById("aboutPortfolio").style.color = '#333333'
+        document.getElementById("aboutPortfolio").style.fontWeight = '500'
+        document.getElementById("aboutPortfolio").style.fontFamily = 'Poppins, sans-serif'
+        // document.getElementById("aboutExperience").style.textDecoration = 'none'
+        // document.getElementById("aboutExperience").style.color = 'rgb(34, 34, 34);'
+    }
 }
 //     if (activeTabs == 'experience') {
 //         document.getElementById("student").style.display = 'none';
